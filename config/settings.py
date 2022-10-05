b76f12da5d933a4d0bcc9307eff32a222e1aadfc
dongseup_lim <dongseup83@gmail.com>
2022-10-05 20:25:20 +0900

    #6.2 Many To Many

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,11 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-CUSTOM_APPS = ["users.apps.UsersConfig"]
+CUSTOM_APPS = [
+    "common.apps.CommonConfig",
+    "users.apps.UsersConfig",
+    "rooms.apps.RoomsConfig",
+]
 
 SYSTEM_APPS = [
     "django.contrib.admin",
