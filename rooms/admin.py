60d3388acbe8df3ce5d0b77a75056c31461187f7
dongseup_lim <dongseup83@gmail.com>
2022-10-18 20:40:47 +0900

    # 8.1 Search Fields

--- rooms/admin.py
+++ rooms/admin.py
@@ -24,6 +24,8 @@ class RoomAdmin(admin.ModelAdmin):
         "updated_at",
     )
 
+    search_fields = ("name", "^price", "=owner__username")
+
 
 @admin.register(Amenity)
 class AmenityAdmin(admin.ModelAdmin):
