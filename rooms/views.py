46120cea2136e492d9ae497d50e7cf8308b1bba9
dongseup_lim <dongseup83@gmail.com>
2022-12-06 10:58:13 +0900

    # 11.23 Create a Booking

--- rooms/views.py
+++ rooms/views.py
@@ -17,7 +17,7 @@ from medias.serializers import PhotoSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.utils import timezone
 from bookings.models import Booking
-from bookings.serializers import PublicBookingSerializer
+from bookings.serializers import PublicBookingSerializer, CreateRoomBookingSerializer
 
 
 class Amenities(APIView):
@@ -228,3 +228,11 @@ class RoomBookings(APIView):
         )
 
         return Response(serializer.data)
+
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        serializer = CreateRoomBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response({"ok": True})
+        else:
+            return Response(serializer.errors)
